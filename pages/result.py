8d97159b3bf6159bef2b9e6caed77e58dc9535ff
michael williams <williamsmikeyj@outlook.com>
2022-06-16 18:14:37 -0400

    add webdriver imports

--- pages/result.py
+++ pages/result.py
@@ -1,4 +1,7 @@
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC, wait
 
 
 class RedBubbleResultPage:
