0229f93da3af2f50fc8c4526ea46479cfada46c5
michael williams <williamsmikeyj@outlook.com>
2022-06-16 13:49:22 -0400

    redbubble result page

---
+++ pages/result.py
@@ -0,0 +1,27 @@
+from selenium.webdriver.common.by import By
+
+
+class RedBubbleResultPage:
+    LINK_DIVS = (By.CSS_SELECTOR_, '#SearchResultsGrid > a')
+    SEARCH_BAR = (By.XPATH, '//form/input')
+
+    @classmethod
+    def PHRASE_RESULTS(cls, phrase):
+        xpath = f"//form/input//*[contains(text(),'{phrase}')]"
+        return (By.XPATH, xpath)
+
+    def __init__(self, browser):
+        self.browser = browser
+
+    def link_div_count(self):
+        link_divs = self.browser.find_elements(*self.LINK_DIVS)
+        return len(link_divs)
+
+    def phrase_result_count(self, phrase):
+        phrase_results = self.browser.find_elements(
+            *self.PHRASE_RESULTS(phrase))
+        return len(phrase_results)
+
+    def search_input_value(self):
+        search_bar = self.browser.find_element(*self.SEARCH_BAR)
+        return search_bar.get_attribute('value')
