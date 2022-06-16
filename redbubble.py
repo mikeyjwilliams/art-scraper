dbd08e87baa86dd57c7dbee2e94d828a8534f03a
michael williams <williamsmikeyj@outlook.com>
2022-06-16 18:17:54 -0400

    add imports for use later

--- redbubble.py
+++ redbubble.py
@@ -2,10 +2,7 @@ from time import sleep
 import chromedriver_autoinstaller
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC, wait
+
 
 from utils.Data import BASE_URL
 
