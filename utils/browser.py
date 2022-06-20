229aec2251d3b67d60080727489944edc50d788a
michael williams <williamsmikeyj@outlook.com>
2022-06-20 13:31:37 -0400

    functions for opening and closing browser

--- utils/browser.py
+++ utils/browser.py
@@ -1,13 +1,18 @@
 from selenium import webdriver
+import chromedriver_autoinstaller
+chromedriver_autoinstaller.install()
 
 
-def browser():
+class Browser:
+    def __init__(self, driver):
+        self.driver = driver
 
-    driver = webdriver.Chrome()
-    return driver
+    def browser(self):
 
+        self.driver = webdriver.Chrome()
+        return self.driver
 
-def close_browser(driver):
+    def close_browser(self):
 
-    driver.close()
-    driver.quit()
+        self.driver.close()
+        self.driver.quit()
