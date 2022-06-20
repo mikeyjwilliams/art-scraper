942857cc39dabddac20fb84a5daa2cff78dd278f
michael williams <williamsmikeyj@outlook.com>
2022-06-20 12:50:58 -0400

    add homepage class

--- pages/homepage.py
+++ pages/homepage.py
@@ -1,3 +1,6 @@
+from time import sleep
+from utils.Data import BASE_URL
+from utils.utility import Base
 
 
 '''
@@ -5,3 +8,13 @@ homepage.py
 goes to home page
 checks its on home page
 '''
+
+
+class HomePage(Base):
+    def __init__(self, driver):
+        super.__init__(self, driver)
+
+        driver = browser()
+        driver.get(BASE_URL)
+        sleep(3)
+        driver.close_browser()
