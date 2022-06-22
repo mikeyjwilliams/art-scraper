0f516c42d1d316455f9b541a37a52aa8c253c4af
michael williams <williamsmikeyj@outlook.com>
2022-06-22 07:50:45 -0400

    add comments to browser.py

--- utils/browser.py
+++ utils/browser.py
@@ -1,18 +1,41 @@
 from selenium import webdriver
-import chromedriver_autoinstaller
-chromedriver_autoinstaller.install()
 
 
 class Browser:
+    '''
+    class Browser
+    parameter: driver
+    '''
+
     def __init__(self, driver):
         self.driver = driver
 
     def browser(self):
+        '''
+        function: browser
+        params: 
+            self
+        returns: 
+            self.driver object
 
+        description: opens chrome browser and returns
+            self.driver object
+            to be called when needed.
+        '''
         self.driver = webdriver.Chrome()
         return self.driver
 
     def close_browser(self):
+        '''
+        function: close_browser
+        params: self
+
+        returns: None
+
+        description: 
+            closes chrome browser
+            quits chrome browser         
+        '''
 
         self.driver.close()
         self.driver.quit()
